fc869ac91519a8a0a4e79f993259881e5af72ddf
Albert Ragány-Németh <me@alb1.hu>
2024-04-21 19:55:15 +0200

    Set up doxygen

--- shell.nix
+++ shell.nix
@@ -6,6 +6,8 @@
         gcc
         gdb
         zip
+        doxygen_gui
+        graphviz
     ];
     shellHook = ''
         export OPEN_DEBUG_PATH=${pkgs.vscode-extensions.ms-vscode.cpptools}/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7;

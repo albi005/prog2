77e5bc96062034e5d77a8cc1441c73dd3c0b7036
Albert Ragány-Németh <me@alb1.hu>
2024-05-02 18:55:10 +0200

    Add non-make debug config

--- .nvim.lua
+++ .nvim.lua
@@ -1,6 +1,13 @@
 local dap = require('dap')
 
-local options = "-g -D MEMTRACE -D DEBUG -O0 -U _FORTIFY_SOURCE --std=c++11"
+local options = "-g -D MEMTRACE -D DEBUG -O0 -U _FORTIFY_SOURCE --std=c++17"
+
+-- !g++ ~/src/prog2/examples/example/*.cpp
+local function compileCmd()
+    local dir = vim.fn.expand('%:h')
+    local cmd = '!g++ ' .. options .. ' ' .. dir .. '/*.cpp'
+    return cmd
+end
 
 dap.configurations.cpp = {
     {
@@ -9,9 +16,18 @@ dap.configurations.cpp = {
         request = "launch",
         program = function()
             local dir = vim.fn.expand('%:p:h')
-            local cmd = '!TARGET=a.out make --environment-overrides --directory ' .. dir
-            vim.cmd(cmd)
-            return dir .. "/a.out"
+            local makefile = dir .. '/Makefile'
+            local useMake = vim.fn.filereadable(makefile)
+            if useMake == 1 then
+                local cmd = '!TARGET=a.out make --environment-overrides --directory ' .. dir
+                vim.cmd(cmd)
+                return dir .. "/a.out"
+            else
+                local cmd = compileCmd()
+                vim.cmd(cmd)
+                local root = vim.fn.getcwd()
+                return root .. "/a.out"
+            end
         end,
         cwd = function()
             local dir = vim.fn.expand('%:p:h')
@@ -21,7 +37,7 @@ dap.configurations.cpp = {
         setupCommands = {
             {
                 text = '-enable-pretty-printing',
-                description =  'enable pretty printing',
+                description = 'enable pretty printing',
                 ignoreFailures = false
             },
         },
@@ -31,24 +47,17 @@ dap.configurations.cpp = {
 dap.adapters.cppdbg = {
     id = 'cppdbg',
     type = 'executable',
-    command = os.getenv'OPEN_DEBUG_PATH',
+    command = os.getenv 'OPEN_DEBUG_PATH',
 }
 
--- !g++ ~/src/prog2/examples/example/*.cpp
-local function compileCmd()
-    local dir = vim.fn.expand('%:h')
-    local cmd = '!g++ ' .. options .. ' '.. dir .. '/*.cpp'
-    return cmd
-end
-
 -- compile and run
-vim.keymap.set('n', '<Leader>r', function ()
+vim.keymap.set('n', '<Leader>r', function()
     local cmd = compileCmd() .. ' && ./a.out'
     vim.cmd(cmd)
 end)
 
 -- compile and run in an interactive shell
-vim.keymap.set('n', '<Leader>t', function ()
+vim.keymap.set('n', '<Leader>t', function()
     vim.cmd(compileCmd())
 
     local subdir = vim.fn.expand('%:p:h')
